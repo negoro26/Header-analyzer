0bcd1900f1e4aa5547207548d0b3b78d452041be
Alex Negrescu <negrescualex05@gmail.com>
2025-09-25 17:17:00 +0300

    added client headers

--- header_scanner.py
+++ header_scanner.py
@@ -1,6 +1,13 @@
 import sys
 import requests
 
+client_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US;q=0.8,en;q=0.3',
+    'Upgrade-Insecure-Requests': '1'
+}
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python header_scanner.py <url>")
@@ -12,8 +19,7 @@ def main():
         url = f"https://{url}"
 
     try:
-        response = requests.get(url)
-        print(f"Found {len(headers)} headers:")
+        response = requests.get(url, headers=client_headers)
         print(response.headers)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
